1223010e0577208ebd32ccff5336fa726d37a3c5
Anudit Nagar <nagaranudit@gmail.com>
2022-06-28 00:37:03 +0530

    Update Subgraph

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # Tachyon Sigma Pass
 
-Build completed: QmZxoq5bUyJ7MRNZmxzNKeweFn1notxE2NnDyPTmdmk3fG
+Build completed: QmQu6r7uSpkvtzLwogbKytm2eXqgpPE3WT3wcgEC3aCo5i
 
-Deployed to https://thegraph.com/studio/subgraph/tachyon-sigma-pass
+Deployed to https://thegraph.com/explorer/subgraph/anudit/tachyon-sigma-pass
 
 Subgraph endpoints:
-Queries (HTTP):     https://api.studio.thegraph.com/query/1649/tachyon-sigma-pass/v1
-Subscriptions (WS): https://api.studio.thegraph.com/query/1649/tachyon-sigma-pass/v1
+Queries (HTTP):     https://api.thegraph.com/subgraphs/name/anudit/tachyon-sigma-pass
+Subscriptions (WS): wss://api.thegraph.com/subgraphs/name/anudit/tachyon-sigma-pass
+
+
+## Examples
+
+### Get Passes
+```
+{
+  passes(first: 100, orderBy: tokenId, orderDirection: desc) {
+    owner
+    tokenId
+    mintedOn
+    txnHash
+    tokenURI
+    metadata {
+      id
+      name
+      description
+      image
+      animation_url
+      attributes
+    }
+  }
+}
+```
--- src/mapping.ts
+++ src/mapping.ts
@@ -2,28 +2,69 @@ import {
   Contract,
   Transfer,
 } from "../generated/Contract/Contract"
-import { Pass } from "../generated/schema"
-import { log } from "@graphprotocol/graph-ts"
+import { Metadata, Pass } from "../generated/schema"
+import { ipfs, JSONValue,  json, JSONValueKind } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
-  let entity = Pass.load(event.params.to.toHexString());
-  log.info('Loaded pass', []);
+  let entity = Pass.load(event.params.tokenId.toString());
   if (entity === null) {
-    entity = new Pass(event.params.to.toHexString());
+    entity = new Pass(event.params.tokenId.toString());
   }
-
-  log.info('Got nft: {}, {}', [event.params.tokenId.toString(), event.transaction.hash.toHexString()])
-
   entity.tokenId = event.params.tokenId;
-  entity.id = event.params.to.toHexString();
+  entity.owner = event.params.to.toHexString();
   entity.mintedOn = event.block.timestamp;
   entity.txnHash = event.transaction.hash;
 
-  // const contract = Contract.bind(event.address);
-  // let tokenUriResp = contract.try_tokenURI(event.params.tokenId);
-  // if(tokenUriResp.reverted == false){
-  //   entity.tokenURI = tokenUriResp.value;
-  // }
+  const contract = Contract.bind(event.address);
+
+  let tokenURI = contract.tokenURI(event.params.tokenId);
+  entity.tokenURI = tokenURI;
+
+  let hash = tokenURI.replaceAll('ipfs://', '');
+  let data = ipfs.cat(hash);
+
+  if (data) {
+    entity.tokenURIRaw = data;
+
+    let jsonData = json.fromBytes(data).toObject();
+    if (jsonData) {
+
+      const metaDataEntity = new Metadata(event.params.tokenId.toString());
+      metaDataEntity.passPointed = event.params.tokenId.toString();
+
+      const name = jsonData.get("name");
+      if (name) metaDataEntity.name = name.toString();
+
+      const description = jsonData.get("description");
+      if (description) metaDataEntity.description = description.toString();
+
+      const image = jsonData.get("image");
+      if (image) metaDataEntity.image = image.toString();
+
+      const animation_url = jsonData.get("animation_url");
+      if (animation_url) metaDataEntity.animation_url = animation_url.toString();
+
+      let attributes = jsonData.get("attributes");
+      if (attributes) {
+        let attrs = attributes.toArray();
+        let attrsParsed: string[] = [];
+
+        for (let index = 0; index < attrs.length; index++) {
+          let item = attrs[index].toObject();
+
+          let t = item.get('value');
+          if (t) {
+            attrsParsed.push(t.toString());
+          }
+        }
+
+        metaDataEntity.attributes = attrsParsed;
+      }
+
+      metaDataEntity.save();
+    }
+
+  };
 
   entity.save();
 }
